49d116a9b98e8d642237633f15200f14a00b910f
Itz-Hyperz <loganfgross@gmail.com>
2023-01-07 22:28:28 -0800

    Update index.js

--- website/index.js
+++ website/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const chalk = require('chalk');
 const fs = require('node:fs');
+const utils = require('hyperz-utils')
 
 // Backend Initialization
 const backend = require('./backend.js');
@@ -11,7 +12,8 @@ backend.init(app);
 
 // Routing
 app.get('', async function(req, res) {
-    res.render('index.ejs');
+    let uploads = JSON.parse(fs.readFileSync(`./uploads.json`));
+    res.render('index.ejs', { uploads: uploads.reverse() });
 });
 
 app.post('/backup', async function(req, res) {
@@ -19,9 +21,20 @@ app.post('/backup', async function(req, res) {
     if(!req.headers.secret) return res.type('json').send(JSON.stringify({"success": false,"info": "No secret was provided in post request headers."}, null, 4) + '\n');
     if(req.headers.secret !== config.apiSecret) return res.type('json').send(JSON.stringify({"success": false,"info": "Secret does not match the secret provided in the EasyBackup config file."}, null, 4) + '\n');
     if(!req.files[0]) return res.type('json').send(JSON.stringify({"success": false,"info": "No file was provided in the post request."}, null, 4) + '\n');
-    req.files.forEach(function(file) {
-        fs.writeFileSync(`./uploads/${file.originalname}`, file.buffer)
+    let time = await utils.fetchTime('EST', 'MM-DD-YYYY hh:mm:ss A');
+    let fileNames = [];
+    for(let file of req.files) {
+        fileNames.push(file.originalname);
+        fs.writeFileSync(`./uploads/${file.originalname}`, file.buffer);
+    };
+    let uploads = JSON.parse(fs.readFileSync(`./uploads.json`));
+    uploads.push({
+        "time": time,
+        "files": fileNames
     });
+    uploads = JSON.stringify(uploads, null, 4) + '\n';
+    fs.writeFileSync(`./uploads.json`, uploads);
+    console.log(`----------------------\nFiles Uploaded Successfully - ${time}`)
     return res.type('json').send(JSON.stringify({"success": true,"info": "Backup success!"}, null, 4) + '\n');
 });
 
