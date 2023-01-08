7fbbb96f48142b727c92605f47fcb0744693b39e
Itz-Hyperz <loganfgross@gmail.com>
2023-01-08 12:35:52 -0800

    yeah lol
    https://cdn.hyperz.net/u/main/TwVbi59.png
    https://cdn.hyperz.net/u/main/5djXT8G.png

--- website/backend.js
+++ website/backend.js
@@ -4,6 +4,7 @@ const multer = require('multer');
 const bodyParser = require('body-parser');
 const session  = require('express-session');
 const express = require("express");
+const requestIp = require('request-ip');
 
 async function init(app) {
     if (Number(process.version.slice(1).split(".")[0] < 16)) throw new Error(`Node.js v16 or higher is required, Discord.JS relies on this version, please update @ https://nodejs.org`);
@@ -25,11 +26,7 @@ async function init(app) {
     app.use(express.static('src/static'));
     app.use('/assets', express.static(__dirname + 'public/assets'));
     app.use('/static', express.static(__dirname + 'src/static/assets'));
-    app.use((req, res, next) => {
-        const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        console.log(`Client IP: ${clientIp}`);
-        next();
-    });
+    app.use(requestIp.mw());
 };
 
 module.exports = {
